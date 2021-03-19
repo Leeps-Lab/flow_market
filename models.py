12a6b993685603e94bd6840b6e543ef4f314caad
alkngo <alexngo2011@hotmail.com>
2021-03-19 08:49:15 -0700

    basic order stuff with live pages and extramodels

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ from otree.api import (
     BasePlayer,
     Currency as c,
     currency_range,
+    ExtraModel
 )
 
 
@@ -30,15 +31,53 @@ class Subsession(BaseSubsession):
 
 class Group(BaseGroup):
     def live_method(self, id_in_group, data):
+        player = self.get_player_by_id(id_in_group)
+        player.set_order(data)
+
         payload = {
-            
+
         }
         return {id_in_group: payload}
 
 
 class Player(BasePlayer):
-    pass
+    # Order states
+    direction = models.StringField() # 'buy', 'sell'
+    max_quantity = models.IntegerField()
+    max_rate = models.FloatField()
+    p_min = models.FloatField()
+    p_max = models.FloatField()
+    status = models.StringField() # 
+
+    def new_order(self, order):
+        self.direction = order['direction']
+        self.max_quantity = order['max_quantity']
+        self.max_rate = order['max_rate']
+        self.p_min = order['p_min']
+        self.p_max = order['p_max']
+        self.status = order['status']
+
+        Order.create(player=self,
+                    group = self.group,
+                    order_timestamp = order['timestamp'],
+                    direction = order['direction'],
+                    max_quantity = order['max_quantity'],
+                    max_rate = order['max_rate'],
+                    p_min = order['p_min'],
+                    p_max = order['p_max'],
+                    status = order['status'])
+    
+    def order_book(self):
+        return Order.filter(player=self)
+
 
 class Order(ExtraModel):
     player = models.Link(Player)
     group = models.Link(Group)
+    order_timestamp = models.FloatField()
+    direction = models.StringField() # 'buy', 'sell'
+    max_quantity = models.IntegerField()
+    max_rate = models.FloatField()
+    p_min = models.FloatField()
+    p_max = models.FloatField()
+    status = models.StringField() # 
