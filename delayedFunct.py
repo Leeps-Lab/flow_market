d08cbe33f5d58cdf596011f7e0027b079694926d
alkngo <alexngo2011@hotmail.com>
2021-06-12 22:53:19 -0700

    normal delayed function

--- delayedFunct.py
+++ delayedFunct.py
@@ -1,5 +1,28 @@
 from .infiniteTimer import InfiniteTimer
+import threading
 from otree.db import idmap
+
+def call_with_delay_infinite(delay: float, callback, *args, **kwargs):
+    """Calls a model method with a specified delay
+
+    Uses Timer to create a delay, then starts a new database session, rebinds the
+    method to a fresh copy of its model and calls it.
+    NOTE: This function will only work when `callback` is a method on some model (Group, Player, etc.).
+    Don't try calling it with a lambda function or something, it won't work."""
+    if delay <= 0:
+        callback(*args, **kwargs)
+        return
+
+    self = callback.__self__
+    cls = type(self)
+
+    def query_and_call():
+        with idmap.use_cache():
+            new_model = cls.objects.get(id=self.id)
+            callback.__func__.__get__(new_model, cls)(*args, **kwargs)
+
+    t = InfiniteTimer(delay, query_and_call)
+    t.start()
 
 def call_with_delay(delay: float, callback, *args, **kwargs):
     """Calls a model method with a specified delay
@@ -20,5 +43,5 @@ def call_with_delay(delay: float, callback, *args, **kwargs):
             new_model = cls.objects.get(id=self.id)
             callback.__func__.__get__(new_model, cls)(*args, **kwargs)
 
-    t = InfiniteTimer(delay, query_and_call)
+    t = threading.Timer(delay, query_and_call)
     t.start()
